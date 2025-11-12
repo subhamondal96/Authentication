3083a9468e1f3e90430bb44486ac09a680570337
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2025-11-12 16:58:50 +0530

    password change

--- database.py
+++ database.py
@@ -2,13 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-<<<<<<< HEAD
 # Replace with your actual MySQL credentials
 DATABASE_URL = "mysql+pymysql://root:Pranab123%40@localhost/transglobal"
-=======
-
-DATABASE_URL = "mysql+pymysql://root:Subha1234@localhost/transglobal"
->>>>>>> b61b1ae3f8165226c065889187c3f7a281172bfe
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
